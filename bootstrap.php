7ffe1d9f5b8ce538130a457cc200c70b4d55e134
manuel <insphare@gmail.com>
2015-04-21 13:57:12 +0200

    merge

---
+++ bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+$ds = DIRECTORY_SEPARATOR;
+
+$arrIncludePath = array(
+	'entity' => __DIR__ . $ds . 'base-entity' . $ds . 'entity',
+);
+
+foreach ($arrIncludePath as $namespace => $includePath) {
+	$autoloader = new \Insphare\Base\Autoloader();
+	$autoloader->addIncludePath($includePath);
+	$autoloader->setNameSpace($namespace);
+	$autoloader->register();
+}
+
+use Insphare\Base\ObjectContainer;
+use Insphare\Base\Application\Setup;
+
+$object = new ObjectContainer();
+/** @var Setup $setup */
+$setup = $object->getSetup();
+$setup->addCustomConfig(__DIR__.DIRECTORY_SEPARATOR.'base-config');
