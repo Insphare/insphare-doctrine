b9b53890a5d42d26b875001cc3f2cc17c5d0294c
Manuel <insphare@gmx.de>
2015-08-31 22:08:04 +0200

    change repo methods to public

--- lib/insphare/ORM/Repository/RepositoryAbstract.php
+++ lib/insphare/ORM/Repository/RepositoryAbstract.php
@@ -21,7 +21,7 @@ abstract class RepositoryAbstract extends EntityRepository {
 	 * @param null|int $limit
 	 * @return QueryBuilder
 	 */
-	protected function cqb($offset = null, $limit = null) {
+	public function cqb($offset = null, $limit = null) {
 		return $this->createQueryBuilder(Util::removeEntityNamespace($this->getEntityName()))->setFirstResult($offset)->setMaxResults($limit);
 	}
 
@@ -30,7 +30,7 @@ abstract class RepositoryAbstract extends EntityRepository {
 	 * @param null $custom
 	 * @return int
 	 */
-	protected function count(QueryBuilder $qb, $custom = null) {
+	public function count(QueryBuilder $qb, $custom = null) {
 		$entityName = Util::removeEntityNamespace($this->getEntityName());
 		$qb->setFirstResult(null)->setMaxResults(null);
 		$qb->select($qb->expr()->count((is_null($custom) ? $entityName : $custom)));
